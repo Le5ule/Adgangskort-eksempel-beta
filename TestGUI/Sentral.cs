7ebdbeb3467ef1927a3b29955d1188ca99eaf682
Leander Surén Levinsen <596237@stud.hvl.no>
2023-10-31 18:50:22 +0100

    Oppgi brukernavn og passord på database. Query til databasen er mot ansatt0

--- TestGUI/Sentral.cs
+++ TestGUI/Sentral.cs
@@ -8,7 +8,7 @@ namespace TestGUI
         Input terminal = new Input();
         //Terminal terminalUI = new Terminal();
 
-        string vstrConnection = "server=129.151.221.119 ; port=5432 ; user id=596237 ; password=Ha1FinDagIDag! ; database=596237 ;";
+        string vstrConnection = "server=129.151.221.119 ; port=5432 ; user id=12345 ; password= ; database= ;";
         NpgsqlConnection vCon;
         NpgsqlCommand vCmd;
 
