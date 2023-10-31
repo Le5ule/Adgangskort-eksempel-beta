91f25f0157e3133ea1c81ef1ad8e44f50ac22851
Leander Surén Levinsen <596237@stud.hvl.no>
2023-10-31 12:53:17 +0100

    Terminalgrensesnitt for innlogging lagt til.

---
+++ TestGUI/Terminal.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace TestGUI
+{
+    partial class Terminal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BTN0 = new Button();
+            BTN9 = new Button();
+            BTN8 = new Button();
+            BTN7 = new Button();
+            BTN6 = new Button();
+            BTN5 = new Button();
+            BTN4 = new Button();
+            BTN3 = new Button();
+            BTN2 = new Button();
+            BTN1 = new Button();
+            BTN_kort = new Button();
+            TB_KomboTerm = new TextBox();
+            SuspendLayout();
+            // 
+            // BTN0
+            // 
+            BTN0.Location = new Point(66, 187);
+            BTN0.Name = "BTN0";
+            BTN0.Size = new Size(23, 25);
+            BTN0.TabIndex = 24;
+            BTN0.Text = "0";
+            BTN0.UseVisualStyleBackColor = true;
+            BTN0.Click += BTN0_Click;
+            // 
+            // BTN9
+            // 
+            BTN9.Location = new Point(93, 159);
+            BTN9.Name = "BTN9";
+            BTN9.Size = new Size(23, 25);
+            BTN9.TabIndex = 23;
+            BTN9.Text = "9";
+            BTN9.UseVisualStyleBackColor = true;
+            BTN9.Click += BTN9_Click;
+            // 
+            // BTN8
+            // 
+            BTN8.Location = new Point(66, 159);
+            BTN8.Name = "BTN8";
+            BTN8.Size = new Size(23, 25);
+            BTN8.TabIndex = 22;
+            BTN8.Text = "8";
+            BTN8.UseVisualStyleBackColor = true;
+            BTN8.Click += BTN8_Click;
+            // 
+            // BTN7
+            // 
+            BTN7.Location = new Point(39, 159);
+            BTN7.Name = "BTN7";
+            BTN7.Size = new Size(23, 25);
+            BTN7.TabIndex = 21;
+            BTN7.Text = "7";
+            BTN7.UseVisualStyleBackColor = true;
+            BTN7.Click += BTN7_Click;
+            // 
+            // BTN6
+            // 
+            BTN6.Location = new Point(93, 130);
+            BTN6.Name = "BTN6";
+            BTN6.Size = new Size(23, 25);
+            BTN6.TabIndex = 20;
+            BTN6.Text = "6";
+            BTN6.UseVisualStyleBackColor = true;
+            BTN6.Click += BTN6_Click;
+            // 
+            // BTN5
+            // 
+            BTN5.Location = new Point(66, 130);
+            BTN5.Name = "BTN5";
+            BTN5.Size = new Size(23, 25);
+            BTN5.TabIndex = 19;
+            BTN5.Text = "5";
+            BTN5.UseVisualStyleBackColor = true;
+            BTN5.Click += BTN5_Click;
+            // 
+            // BTN4
+            // 
+            BTN4.Location = new Point(39, 130);
+            BTN4.Name = "BTN4";
+            BTN4.Size = new Size(23, 25);
+            BTN4.TabIndex = 18;
+            BTN4.Text = "4";
+            BTN4.UseVisualStyleBackColor = true;
+            BTN4.Click += BTN4_Click;
+            // 
+            // BTN3
+            // 
+            BTN3.Location = new Point(93, 101);
+            BTN3.Name = "BTN3";
+            BTN3.Size = new Size(23, 25);
+            BTN3.TabIndex = 17;
+            BTN3.Text = "3";
+            BTN3.UseVisualStyleBackColor = true;
+            BTN3.Click += BTN3_Click;
+            // 
+            // BTN2
+            // 
+            BTN2.Location = new Point(66, 101);
+            BTN2.Name = "BTN2";
+            BTN2.Size = new Size(23, 25);
+            BTN2.TabIndex = 16;
+            BTN2.Text = "2";
+            BTN2.UseVisualStyleBackColor = true;
+            BTN2.Click += BTN2_Click;
+            // 
+            // BTN1
+            // 
+            BTN1.Location = new Point(39, 101);
+            BTN1.Name = "BTN1";
+            BTN1.Size = new Size(23, 25);
+            BTN1.TabIndex = 15;
+            BTN1.Text = "1";
+            BTN1.UseVisualStyleBackColor = true;
+            BTN1.Click += BTN1_Click;
+            // 
+            // BTN_kort
+            // 
+            BTN_kort.Location = new Point(39, 43);
+            BTN_kort.Name = "BTN_kort";
+            BTN_kort.Size = new Size(101, 26);
+            BTN_kort.TabIndex = 25;
+            BTN_kort.Text = "*Tap kort*";
+            BTN_kort.UseVisualStyleBackColor = true;
+            // 
+            // TB_KomboTerm
+            // 
+            TB_KomboTerm.Location = new Point(280, 157);
+            TB_KomboTerm.Name = "TB_KomboTerm";
+            TB_KomboTerm.ReadOnly = true;
+            TB_KomboTerm.Size = new Size(125, 27);
+            TB_KomboTerm.TabIndex = 26;
+            // 
+            // Terminal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TB_KomboTerm);
+            Controls.Add(BTN_kort);
+            Controls.Add(BTN0);
+            Controls.Add(BTN9);
+            Controls.Add(BTN8);
+            Controls.Add(BTN7);
+            Controls.Add(BTN6);
+            Controls.Add(BTN5);
+            Controls.Add(BTN4);
+            Controls.Add(BTN3);
+            Controls.Add(BTN2);
+            Controls.Add(BTN1);
+            Name = "Terminal";
+            Text = "Terminal";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button BTN0;
+        private Button BTN9;
+        private Button BTN8;
+        private Button BTN7;
+        private Button BTN6;
+        private Button BTN5;
+        private Button BTN4;
+        private Button BTN3;
+        private Button BTN2;
+        private Button BTN1;
+        private Button BTN_kort;
+        private TextBox TB_KomboTerm;
+    }
+}
---
+++ TestGUI/Terminal.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestGUI
+{
+    public partial class Terminal : Form
+    {
+        Input autentisering = new Input();
+        Sentral sentralUI = new Sentral();
+
+        public Terminal()
+        {
+            InitializeComponent();
+        }
+        public void access(int inn)
+        {
+            autentisering.Kode().Add(inn);
+
+            if (autentisering.Kode().Count == 4)
+            {
+                autentisering.Godkjenning(autentisering.Kombo());
+                TB_KomboTerm.Text = autentisering.Kombo();
+                //this.Hide();
+                sentralUI.Show();
+                autentisering.Kode().Clear();
+                
+            }
+        }
+
+        private void BTN1_Click(object sender, EventArgs e)
+        {
+            access(1);
+        }
+
+        private void BTN2_Click(object sender, EventArgs e)
+        {
+            access(2);
+        }
+
+        private void BTN3_Click(object sender, EventArgs e)
+        {
+            access(3);
+        }
+
+        private void BTN4_Click(object sender, EventArgs e)
+        {
+            access(4);
+        }
+
+        private void BTN5_Click(object sender, EventArgs e)
+        {
+            access(5);
+        }
+
+        private void BTN6_Click(object sender, EventArgs e)
+        {
+            access(6);
+        }
+
+        private void BTN7_Click(object sender, EventArgs e)
+        {
+            access(7);
+        }
+
+        private void BTN8_Click(object sender, EventArgs e)
+        {
+            access(8);
+        }
+
+        private void BTN9_Click(object sender, EventArgs e)
+        {
+            access(9);
+        }
+
+        private void BTN0_Click(object sender, EventArgs e)
+        {
+            access(0);
+        }
+    }
+}
